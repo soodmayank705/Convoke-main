38fdbdc0d5cfd35eb7b6f93f952d857dcb819dbc
Mayank Sood <soodmayank705@gmail.com>
2017-03-15 19:47:19 +0530

    First commit

---
+++ scheduling.php
@@ -0,0 +1,26 @@
+ <?php
+
+
+
+$competitons[1]=array();
+
+
+
+  $competitons = array("");
+ $arrlength=count($competitons);
+
+ for($x=0; $x<$arlength; $x++)
+ {
+
+  echo $copmetitipns=[$x];
+  echo "<br>";
+
+
+ }
+
+
+?>
+
+
+
+
